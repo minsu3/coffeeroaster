79f4be3df0c82942a660c233a54b9ae49b186cbd
Min Su Kim <kim97minsu@gmail.com>
2021-05-03 20:37:58 -0700

    updated select option, modal, and summary

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,49 @@
-// function setListColor() {
-//   let elementClass = document.getElementB;
-// }
+let allSelected = {};
+
+function selectOption(id, elementClassName) {
+  let selected = document.getElementById(id).firstElementChild.textContent;
+  allSelected[elementClassName] = selected;
+}
+
+function openModal() {
+  document.getElementById("modal").style.display = "block";
+  let orderSummary = document.getElementById("orderSummary").textContent;
+  console.log(orderSummary); // the sentence
+  document.getElementById("sentence").innerHTML = orderSummary;
+
+  let keywords = ["Every week", "Every 2 weeks", "Every month"];
+  const matched = [];
+  for (let index = 0; index < orderSummary.length; index++) {
+    for (let i = 0; i < keywords.length; i++) {
+      if (orderSummary[index].includes(keywords[i])) {
+        matched.push(keywords[i]);
+      }
+    }
+  }
+  console.log("The matched keywords are =", matched);
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+function fillInSummary(elementClassName) {
+  if (elementClassName === "preference-options") {
+    document.getElementById("one").innerHTML = allSelected[elementClassName];
+  }
+  if (elementClassName === "type-options") {
+    document.getElementById("two").innerHTML = allSelected[elementClassName];
+  }
+  if (elementClassName === "quantity-options") {
+    document.getElementById("three").innerHTML = allSelected[elementClassName];
+  }
+  if (elementClassName === "grind-options") {
+    document.getElementById("four").innerHTML = allSelected[elementClassName];
+  }
+  if (elementClassName === "delivery-options") {
+    document.getElementById("five").innerHTML = allSelected[elementClassName];
+  }
+}
 
 function selectCardColor(id) {
   let elementClassName = document.getElementById(id).className;
@@ -19,6 +62,8 @@ function selectCardColor(id) {
       });
     }
   }
+  selectOption(id, elementClassName);
+  fillInSummary(elementClassName);
 }
 
 function showTypeOfCoffee(id) {
@@ -46,5 +91,9 @@ function showDelivery(id) {
   document.getElementById("delivery").style.display = "flex";
   document.getElementById("grindFour").style.color = "rgb(173, 173, 173)";
   document.getElementById("deliveryFive").style.color = "black";
+  selectCardColor(id);
+}
+
+function selectDelivery(id) {
   selectCardColor(id);
 }
